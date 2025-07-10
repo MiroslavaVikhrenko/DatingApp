82b4c777e7d16c795cf704c882bf1ce9b11b8b56
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-07-10 14:59:03 -0600

    Add updating resources

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +31,31 @@ namespace API.Controllers
         public async Task<ActionResult<IReadOnlyList<Photo>>> GetMemberPhotos(string id)
         {
             return Ok(await memberRepository.GetPhotosForMemberAsync(id));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+        {
+            var memberId = User.GetMemberId();
+
+            var member = await memberRepository.GetMemberForUpdate(memberId);
+
+            if (member == null) return BadRequest("Could not get member");
+
+            member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+            member.Description = memberUpdateDto.Description ?? member.Description;
+            member.City = memberUpdateDto.City ?? member.City;
+            member.Country = memberUpdateDto.Country ?? member.Country;
+
+            member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+            // if we wnat to ensure that we're not going to to return a bad request because the member
+            // hasn't changed from what's been saved into the db 
+            memberRepository.Update(member); // Optional
+
+            if (await memberRepository.SaveAllAsync()) return NoContent(); // 204 response for put request
+
+            return BadRequest("Failed to update member");
         }
     }
 }
